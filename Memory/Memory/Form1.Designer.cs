25ee6c2f99360bcca2d01a3a1cf99c297b2dd8ca
Pugnar <swen.vd.wijngaard@gmail.com>
2017-10-09 12:47:16 +0200

    Buttons
    play button en grid buttons

--- Memory/Memory/Form1.Designer.cs
+++ Memory/Memory/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Memory
+﻿namespace WindowsFormsApp1
 {
     partial class Form1
     {
@@ -28,12 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.GridButton1 = new System.Windows.Forms.Button();
+            this.GridButton2 = new System.Windows.Forms.Button();
+            this.GridButton3 = new System.Windows.Forms.Button();
+            this.GridButton4 = new System.Windows.Forms.Button();
+            this.GridButton5 = new System.Windows.Forms.Button();
+            this.GridButton6 = new System.Windows.Forms.Button();
+            this.GridButton7 = new System.Windows.Forms.Button();
+            this.GridButton8 = new System.Windows.Forms.Button();
+            this.GridButton9 = new System.Windows.Forms.Button();
+            this.GridButton10 = new System.Windows.Forms.Button();
+            this.GridButton11 = new System.Windows.Forms.Button();
+            this.GridButton12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(133, 177);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(156, 39);
+            this.PlayButton.TabIndex = 0;
+            this.PlayButton.Text = "Play";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // GridButton1
+            // 
+            this.GridButton1.Location = new System.Drawing.Point(52, 59);
+            this.GridButton1.Name = "GridButton1";
+            this.GridButton1.Size = new System.Drawing.Size(75, 23);
+            this.GridButton1.TabIndex = 1;
+            this.GridButton1.Text = "button1";
+            this.GridButton1.UseVisualStyleBackColor = true;
+            // 
+            // GridButton2
+            // 
+            this.GridButton2.Location = new System.Drawing.Point(133, 59);
+            this.GridButton2.Name = "GridButton2";
+            this.GridButton2.Size = new System.Drawing.Size(75, 23);
+            this.GridButton2.TabIndex = 2;
+            this.GridButton2.Text = "button2";
+            this.GridButton2.UseVisualStyleBackColor = true;
+            // 
+            // GridButton3
+            // 
+            this.GridButton3.Location = new System.Drawing.Point(214, 59);
+            this.GridButton3.Name = "GridButton3";
+            this.GridButton3.Size = new System.Drawing.Size(75, 23);
+            this.GridButton3.TabIndex = 3;
+            this.GridButton3.Text = "button3";
+            this.GridButton3.UseVisualStyleBackColor = true;
+            // 
+            // GridButton4
+            // 
+            this.GridButton4.Location = new System.Drawing.Point(295, 59);
+            this.GridButton4.Name = "GridButton4";
+            this.GridButton4.Size = new System.Drawing.Size(75, 23);
+            this.GridButton4.TabIndex = 4;
+            this.GridButton4.Text = "button4";
+            this.GridButton4.UseVisualStyleBackColor = true;
+            // 
+            // GridButton5
+            // 
+            this.GridButton5.Location = new System.Drawing.Point(52, 88);
+            this.GridButton5.Name = "GridButton5";
+            this.GridButton5.Size = new System.Drawing.Size(75, 23);
+            this.GridButton5.TabIndex = 5;
+            this.GridButton5.Text = "button5";
+            this.GridButton5.UseVisualStyleBackColor = true;
+            // 
+            // GridButton6
+            // 
+            this.GridButton6.Location = new System.Drawing.Point(133, 88);
+            this.GridButton6.Name = "GridButton6";
+            this.GridButton6.Size = new System.Drawing.Size(75, 23);
+            this.GridButton6.TabIndex = 6;
+            this.GridButton6.Text = "button6";
+            this.GridButton6.UseVisualStyleBackColor = true;
+            // 
+            // GridButton7
+            // 
+            this.GridButton7.Location = new System.Drawing.Point(214, 88);
+            this.GridButton7.Name = "GridButton7";
+            this.GridButton7.Size = new System.Drawing.Size(75, 23);
+            this.GridButton7.TabIndex = 7;
+            this.GridButton7.Text = "button7";
+            this.GridButton7.UseVisualStyleBackColor = true;
+            // 
+            // GridButton8
+            // 
+            this.GridButton8.Location = new System.Drawing.Point(295, 90);
+            this.GridButton8.Name = "GridButton8";
+            this.GridButton8.Size = new System.Drawing.Size(75, 23);
+            this.GridButton8.TabIndex = 8;
+            this.GridButton8.Text = "button8";
+            this.GridButton8.UseVisualStyleBackColor = true;
+            // 
+            // GridButton9
+            // 
+            this.GridButton9.Location = new System.Drawing.Point(52, 118);
+            this.GridButton9.Name = "GridButton9";
+            this.GridButton9.Size = new System.Drawing.Size(75, 23);
+            this.GridButton9.TabIndex = 9;
+            this.GridButton9.Text = "button9";
+            this.GridButton9.UseVisualStyleBackColor = true;
+            // 
+            // GridButton10
+            // 
+            this.GridButton10.Location = new System.Drawing.Point(133, 118);
+            this.GridButton10.Name = "GridButton10";
+            this.GridButton10.Size = new System.Drawing.Size(75, 23);
+            this.GridButton10.TabIndex = 10;
+            this.GridButton10.Text = "button10";
+            this.GridButton10.UseVisualStyleBackColor = true;
+            // 
+            // GridButton11
+            // 
+            this.GridButton11.Location = new System.Drawing.Point(214, 118);
+            this.GridButton11.Name = "GridButton11";
+            this.GridButton11.Size = new System.Drawing.Size(75, 23);
+            this.GridButton11.TabIndex = 11;
+            this.GridButton11.Text = "button11";
+            this.GridButton11.UseVisualStyleBackColor = true;
+            // 
+            // GridButton12
+            // 
+            this.GridButton12.Location = new System.Drawing.Point(295, 119);
+            this.GridButton12.Name = "GridButton12";
+            this.GridButton12.Size = new System.Drawing.Size(75, 23);
+            this.GridButton12.TabIndex = 12;
+            this.GridButton12.Text = "button12";
+            this.GridButton12.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 271);
+            this.Controls.Add(this.GridButton12);
+            this.Controls.Add(this.GridButton11);
+            this.Controls.Add(this.GridButton10);
+            this.Controls.Add(this.GridButton9);
+            this.Controls.Add(this.GridButton8);
+            this.Controls.Add(this.GridButton7);
+            this.Controls.Add(this.GridButton6);
+            this.Controls.Add(this.GridButton5);
+            this.Controls.Add(this.GridButton4);
+            this.Controls.Add(this.GridButton3);
+            this.Controls.Add(this.GridButton2);
+            this.Controls.Add(this.GridButton1);
+            this.Controls.Add(this.PlayButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Button GridButton1;
+        private System.Windows.Forms.Button GridButton2;
+        private System.Windows.Forms.Button GridButton3;
+        private System.Windows.Forms.Button GridButton4;
+        private System.Windows.Forms.Button GridButton5;
+        private System.Windows.Forms.Button GridButton6;
+        private System.Windows.Forms.Button GridButton7;
+        private System.Windows.Forms.Button GridButton8;
+        private System.Windows.Forms.Button GridButton9;
+        private System.Windows.Forms.Button GridButton10;
+        private System.Windows.Forms.Button GridButton11;
+        private System.Windows.Forms.Button GridButton12;
     }
 }
 
--- Memory/Memory/Form1.cs
+++ Memory/Memory/Form1.cs
@@ -8,13 +8,40 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Memory
+namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            Button[] ButtonGrid = {GridButton1, GridButton2, GridButton3, GridButton4, GridButton5, GridButton6, GridButton7, GridButton8, GridButton9, GridButton10, GridButton11, GridButton12};
+
+            foreach (var x in ButtonGrid)
+            {
+                x.Visible = false;
+            }
+
+
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void PlayButton_Click(object sender, EventArgs e)
+        {
+            Button[] ButtonGrid = { GridButton1, GridButton2, GridButton3, GridButton4, GridButton5, GridButton6, GridButton7, GridButton8, GridButton9, GridButton10, GridButton11, GridButton12 };
+            foreach (var x in ButtonGrid)
+            {
+                x.Visible = true;
+                PlayButton.Visible = false;
+            }
         }
     }
 }
