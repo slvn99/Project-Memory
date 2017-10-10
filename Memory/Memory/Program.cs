a9926162085d58d0b85a70c5b945ed9f3c84d156
slvn99 <slvn99@gmail.com>
2017-10-10 13:10:28 +0200

    Half gare .sav class
    Niet getest, niet gecustomized, niet netjes, zonder fatsoenlijke invoer, met halvegaar werk

--- Memory/Memory/Program.cs
+++ Memory/Memory/Program.cs
@@ -2,21 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
