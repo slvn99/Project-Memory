8d89fde6f6d568eb1c2331822a366b6610c12322
slvn99 <slvn99@gmail.com>
2017-10-11 16:16:36 +0200

    DIE FKN SAVE WERKT
    HIJ READ
    HIJ WRITE
    HIJ DISPLAYED
    ALLLES IS GUCCI
    Alleen nog koppelen aan gebruikte variabelen en meer aanroepmogelijkheden

--- Memory/Memory/Save.cs
+++ Memory/Memory/Save.cs
@@ -29,7 +29,7 @@ namespace WindowsFormsApp1
         
         public static string SaveData()
         {
-            string output = "";
+            
             Save save = new Save();
             //invoer van naam + lege turn + lege score
             int icounter = 0, score = 0;
@@ -38,27 +38,26 @@ namespace WindowsFormsApp1
             //omzetten naar bytes
             byte[] serialized = Serialize(icounter,score,speler_1);
 
-            WriteToFile(@"C:\Users\svnoo\Documents\GitHub\Project-Memory\Memory\Memory\Savegames", serialized);
+            WriteToFile(@"C:\Users\svnoo\Documents\GitHub\Project-Memory\Memory\Memory\Savegames\game.sav", serialized);
 
 
             //het ophalen van de bytes uit de .sav
-            byte[] bytes = ReadFromFile(@"C:\Users\svnoo\Documents\GitHub\Project-Memory\Memory\Memory\Savegames");
+            byte[] bytes = ReadFromFile(@"C:\Users\svnoo\Documents\GitHub\Project-Memory\Memory\Memory\Savegames\game.sav");
 
             //Terugzetten van bytes naar data
-            GameData deserialized = Deserialize(bytes);
+            string opslag = Deserialize(bytes);
 
-            //Writen van de variabelen
-            output = deserialized.Naam1 + "/n" + deserialized.TurnCounter + "/n" + deserialized.Score;
-            return (output);
+            //variabelen teruggeven aan button die een label aanpast
+            return (opslag);
         }
 
         private static byte[] Serialize(int data1,int data2, string data3)
         {
-            //Maak een nieuwe memory stream aan, die wordt gebruikt door de binary formatter.
+            //Nieuwe memory stream aanmaken die wordt gebruikt door de formatter
             //De 'using' zorgt er voor dat de memory stream altijd correct wordt afgesloten.
             using (MemoryStream stream = new MemoryStream())
             {
-                //We maken een binary formatter aan en zeggen dat hij de data moet serializen (output wordt in de memory stream gezet).
+                //Binary formatter die de data serialized, en dit in de stream zet
                 BinaryFormatter formatter = new BinaryFormatter();
                 formatter.Serialize(stream, data1);
                 formatter.Serialize(stream, data2);
@@ -69,27 +68,35 @@ namespace WindowsFormsApp1
             }
         }
 
-        private static GameData Deserialize(byte[] data)
+        private static string Deserialize(byte[] data)
         {
-            //Maak een nieuwe memory stream aan, die wordt gebruikt door de binary formatter.
+            //Nieuwe memory stream aanmaken die wordt gebruikt door de formatter
             //De 'using' zorgt er voor dat de memory stream altijd correct wordt afgesloten.
-            //We zetten het data wat we van de uit het bestand hebben gelezen in de memory stream.
+            //De data uit de .sav wordt in de stream gezet
             using (MemoryStream stream = new MemoryStream(data))
             {
-                //We maken een binary formatter aan en zeggen dat hij de data moet serializen (output wordt in de memory stream gezet).
+                //Binary formatter die de data deserialized, en dit in de stream zet
                 BinaryFormatter formatter = new BinaryFormatter();
 
                 //Return de game data.
-                return (GameData)formatter.Deserialize(stream);
+                var d1 = formatter.Deserialize(stream);
+                var d2 = formatter.Deserialize(stream);
+                var d3 = formatter.Deserialize(stream);
+                string buf1,buf2,buf3,opslag;
+                buf1 = Convert.ToString(d1);
+                buf2 = Convert.ToString(d2);
+                buf3 = Convert.ToString(d3);
+                opslag = (buf1 + "\n" + buf2 + "\n" + buf3);
+                return opslag;
             }
         }
 
         private static void WriteToFile(string file, byte[] data)
         {
-            //Try catch block om eventuele errors af te vangen.
+            //Try catch block om eventuele errors op te vangen.
             try
             {
-                //Schrijf all bytes naar het opgegeven path.
+                //Write van bytes naar opgegeven path
                 File.WriteAllBytes(file, data);
             }
             catch (Exception e)
@@ -103,7 +110,7 @@ namespace WindowsFormsApp1
             //Try catch block om eventuele errors af te vangen.
             try
             {
-                //Alle bytes uitlezen uit een bestand en deze returnen.
+                //Read van bytes vanuit opgegeven path
                 byte[] bytes = File.ReadAllBytes(file);
                 return bytes;
             }
