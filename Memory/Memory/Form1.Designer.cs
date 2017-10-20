d24abc9ca439eaec9cd20300597afd94db8472f2
slvn99 <slvn99@gmail.com>
2017-10-20 11:15:03 +0200

    commit before pull

--- Memory/Memory/Form1.Designer.cs
+++ Memory/Memory/Form1.Designer.cs
@@ -585,7 +585,6 @@ namespace WindowsFormsApp1
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.play)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Reset)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
