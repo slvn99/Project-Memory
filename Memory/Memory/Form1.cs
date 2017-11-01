7df5f34478b0f98d4197c4eb64e6f7871140626a
Frank <jong1722@student.nhl.nl>
2017-11-01 12:32:10 +0100

    flikker fix

--- Memory/Memory/Form1.cs
+++ Memory/Memory/Form1.cs
@@ -882,6 +882,15 @@ namespace WindowsFormsApp1
             Dispose();
             GC.Collect();
         }
-    }
 
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				CreateParams cp = base.CreateParams;
+				cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+				return cp;
+			}
+		}
+	}
 }
