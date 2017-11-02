fcb785af8c6eec2a7cb240fba0c6aec0810fbe47
Frank <jong1722@student.nhl.nl>
2017-11-02 12:06:05 +0100

    Ja moet van sam pushen

--- Memory/Memory/Properties/Resources.Designer.cs
+++ Memory/Memory/Properties/Resources.Designer.cs
@@ -901,6 +901,16 @@ namespace Memory.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap StopMusicButton {
+            get {
+                object obj = ResourceManager.GetObject("StopMusicButton", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         public static System.IO.UnmanagedMemoryStream THE_TING_GO_SKRRA {
