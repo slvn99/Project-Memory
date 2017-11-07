2b53bed4cac6a11104505fa119505c70e40f64f8
Pugnar <swen.vd.wijngaard@gmail.com>
2017-11-07 13:43:21 +0100

    Server Updates

--- Memory/Memory/GameServer.cs
+++ Memory/Memory/GameServer.cs
@@ -18,7 +18,7 @@ namespace Memory
         int PuntenLocalPlayer, PuntenOtherPlayer, TotaalMatches;
         System.Media.SoundPlayer player = new System.Media.SoundPlayer();
         public static Label TempConLabel = new Label();
-        Button Kaart1Select, Kaart2Select, TempKaart1Select, TempKaart2Select;
+        Button Kaart1Select, Kaart2Select;
         bool host = false;
         List<Point> PointLocation = new List<Point>();
         public static List<Point> RandomButLocation = new List<Point>();
@@ -53,6 +53,11 @@ namespace Memory
                 cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
                 return cp;
             }
+        }
+
+        internal static void CloseApplication()
+        {
+            GameServer.CloseApplication();
         }
 
         void RandomizeButtons()
@@ -707,6 +712,7 @@ namespace Memory
             {
                 MessageBox.Show("ERROR, Connectie timed out", "Time Out", MessageBoxButtons.OK);
                 ServerHost.Listener.Stop();
+                ClientButton.Enabled = true;
             }
             else
             {
--- Memory/Memory/ServerClient.cs
+++ Memory/Memory/ServerClient.cs
@@ -44,15 +44,33 @@ namespace Memory
 
         public static void SendTurn()
         {
-            TurnArray = GameServer.TurnArray;
-            var bin = new BinaryFormatter();
-            bin.Serialize(Client.GetStream(), TurnArray);
+            try
+            {
+                TurnArray = GameServer.TurnArray;
+                var bin = new BinaryFormatter();
+                bin.Serialize(Client.GetStream(), TurnArray);
+            }
+            catch
+            {
+                MessageBox.Show("Error, Connectie verloren!", "ERROR!", MessageBoxButtons.OK);
+                Client.Close();
+                GameServer.CloseApplication();
+            }
         }
 
         public static void RecieveTurn()
         {
-            var bin = new BinaryFormatter();
-            TurnArray =(string[])bin.Deserialize(Client.GetStream());
+            try
+            {
+                var bin = new BinaryFormatter();
+                TurnArray = (string[])bin.Deserialize(Client.GetStream());
+            }
+            catch
+            {
+                MessageBox.Show("Error, Connectie verloren!", "ERROR!", MessageBoxButtons.OK);
+                Client.Close();
+                GameServer.CloseApplication();
+            }
         }
 
         public static void SendName()
@@ -73,28 +91,6 @@ namespace Memory
             byte[] bytes = Encoding.UTF8.GetBytes(message);
             NetworkStream stream = Client.GetStream();
             stream.Write(bytes, 0, bytes.Length);
-        }
-
-        public static string ReceiveMessage()
-        {
-            byte[] bytes = new byte[PacketSize];
-            NetworkStream stream = Client.GetStream();
-            stream.Read(bytes, 0, PacketSize);
-            return cleanMessage(bytes);
-        }
-        private static string cleanMessage(byte[] bytes)
-        {
-            string message = Encoding.UTF8.GetString(bytes);
-
-            string messageToPrint = null;
-            foreach (var nullChar in message)
-            {
-                if (nullChar != '\0')
-                {
-                    messageToPrint += nullChar;
-                }
-            }
-            return messageToPrint;
         }
     }
 }
